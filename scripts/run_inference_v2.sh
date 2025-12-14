18830b59f7d36023c2d88cee8a728e2048cf474c
LIarLiar123454321 <Lester20051213@gmail.com>
2025-12-14 22:51:36 +0800

    Add shell error handling and logging framework

--- scripts/run_inference_v2.sh
+++ scripts/run_inference_v2.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -e
 
+#shell 自動印出錯誤幾行和指令
+trap 'echo "[ERROR] 發生錯誤，行號 $LINENO，指令：$BASH_COMMAND"; exit 1' ERR
+
 MODEL_PATH="models/pd_v1.eim"
+
+#如果eim還沒匯入
+if [ ! -f "$MODEL_PATH" ]; then
+  echo "[ERROR] script=run_inference_v2.sh step=check_model message=\"model not found: $MODEL_PATH\>"
+  exit 2
+fi
+
+
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 OUTPUT_DIR="results/${TIMESTAMP}"
 mkdir -p "$OUTPUT_DIR"
+
+
+
 
 for img in data/test/*.{jpg,JPG,jpeg,JPEG}; do
     [ -f "$img" ] || continue  # 沒有匹配檔案就跳過
@@ -13,6 +27,5 @@ for img in data/test/*.{jpg,JPG,jpeg,JPEG}; do
     echo "[INFO] 推論 $img → $OUTPUT_FILE"
     python3 scripts/run3.py "$MODEL_PATH" "$img" "$OUTPUT_FILE"
 done
-
 echo "[INFO] 批次推論完成，結果都在 $OUTPUT_DIR"
 
